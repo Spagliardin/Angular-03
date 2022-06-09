4cf52ce57d13025dc218508670ce4c0b1c9dcd1d
Nicolas Spagliardi <nspagliardi@emergencias.com.ar>
2022-06-09 10:34:44 -0300

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppRoutingModule } from './app-routing-module';
 import { SharedModule } from './shared/shared.module';
 import { CountryModule } from './country/country.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,12 @@ import { AppComponent } from './app.component';
 
     CountryModule,
     SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
